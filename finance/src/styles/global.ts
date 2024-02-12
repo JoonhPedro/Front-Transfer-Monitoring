7d63a383f9f0fea4b97d43fe8807e91b1c016024
John <joao.veo77@com.br>
2024-02-12 15:53:10 +0000

    styling correction

--- finance/src/styles/global.ts
+++ finance/src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     *{
@@ -9,11 +9,11 @@ export const GlobalStyle = createGlobalStyle`
     }
     :focus{
         outline: 0;
-        box-shadow: 0 8 0 2px ${props => props.theme['green-500']};
+        box-shadow: 0 8 0 2px ${(props) => props.theme['green-500']};
     }
     body{
-        background-color: ${props => props.theme['grey-800']};
-        color: ${props => props.theme['grey-100']};
+        background: ${(props) => props.theme['gray-800']};
+        color: ${(props) => props.theme['gray-300']};
         -webkit-font-smoothing: antialiased;
     }
     border-style,input-security,text-decoration-thickness,button {
