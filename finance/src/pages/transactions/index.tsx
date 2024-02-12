f8339a738d197c78c1020690ab422929a2ffb4c3
John <joao.veo77@com.br>
2024-02-12 15:50:15 +0000

    import transactions added and table created

--- finance/src/pages/transactions/index.tsx
+++ finance/src/pages/transactions/index.tsx
@@ -1,7 +1,46 @@
-import { Header } from "../../components/Header"
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SerchForm'
 
-export function Transactions () {
-    return(
-        <div></div>
-    )
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './style'
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Pizza</td>
+              <td>
+                <PriceHighLight variant="outcome">R$ - 59,00</PriceHighLight>
+              </td>
+
+              <td>Alimentação</td>
+              <td>11/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
 }
